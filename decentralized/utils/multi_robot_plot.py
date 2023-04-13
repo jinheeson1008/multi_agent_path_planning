d4c7978e21f35f0f716f5e6d5a5d73783ac7ee9a
Jinhee Son <sn00055@gmail.com>
2023-04-14 01:29:49 +0900

    Update multi_robot_plot.py

--- decentralized/utils/multi_robot_plot.py
+++ decentralized/utils/multi_robot_plot.py
@@ -113,18 +113,19 @@ def plot_robot_obstacles_and_vos(robot, obstacles, robot_radius, num_steps, sim_
         robot_patch.center = (robot[0, i], robot[1, i])
 
         for j in range(len(vo_cone_list)):
-            x_vo_line = np.arange(0,20)
+            x_vo_line = np.arange(-20,20)
             vo_cone_handle=vo_cone_list[j]
             assert isinstance(vo_cone_handle,Wedge)
             vo_cone_handle.set_center((vo_pt_hist[j,0,i],vo_pt_hist[j,1,i]))
             vo_cone_handle.set_radius(vo_dist_hist[j,0,i]*100)
             vo_cone_handle.set_alpha(0.5)
+            obs_loc=(obstacles[0, i, j], obstacles[1, i, j])
             angle_2 =np.rad2deg(np.arctan2(vo_Amat_hist[2*j+1,1,i],vo_Amat_hist[2*j+1,0,i]))
             angle_1 =np.rad2deg(np.arctan2(vo_Amat_hist[2*j,1,i],vo_Amat_hist[2*j,0,i]))
             angle_not_reverse = angle_2-angle_1<180 if (angle_2-angle_1>=0) else angle_2-angle_1+360 < 180
             if(1):
-                vo_cone_handle.set_theta1(np.rad2deg(np.arctan2(1*vo_Amat_hist[2*j+1,0,i],-1*vo_Amat_hist[2*j+1,1,i])))
-                vo_cone_handle.set_theta2(np.rad2deg(np.arctan2(1*vo_Amat_hist[2*j,0,i],-1*vo_Amat_hist[2*j,1,i])))
+                vo_cone_handle.set_theta2(np.rad2deg(np.arctan2(1*vo_Amat_hist[2*j+1,0,i],-1*vo_Amat_hist[2*j+1,1,i])))
+                vo_cone_handle.set_theta1(np.rad2deg(np.arctan2(1*vo_Amat_hist[2*j,0,i],-1*vo_Amat_hist[2*j,1,i])))
             else:
                 vo_cone_handle.set_theta1(np.rad2deg(np.arctan2(vo_Amat_hist[2*j+1,1,i],1*vo_Amat_hist[2*j+1,0,i])))
                 vo_cone_handle.set_theta2(np.rad2deg(np.arctan2(vo_Amat_hist[2*j,1,i],1*vo_Amat_hist[2*j,0,i])))
